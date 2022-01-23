82216c0483e3be9965f249aa47c3861dfa2579dc
tyesc <escolano.tyler@gmail.com>
2022-01-23 10:40:39 +0100

    refactor: build

--- script/build.js
+++ script/build.js
@@ -10,25 +10,6 @@ const { once } = require('ramda');
 const webpackConfig = require('../webpack.config.js');
 const listServices = require('./list-services');
 
-if (!fs.existsSync(path.resolve('.env'))) {
-  console.error(colors.red(
-    '[blinddeez.serve] No .env file found, please run `yarn getenv @dev` ' +
-    'to be able to serve.',
-  ));
-  process.exit(1);
-}
-
-const envFile = fs.readFileSync(path.resolve('.env'), { encoding: 'utf8' });
-
-if (envFile.indexOf('# DEV') === -1 && !argv.force) {
-  console.error(colors.red(
-    '[blinddeez.serve] Your .env file doesn\'t seem to be meant for ' +
-    ' development environment. Please run `yarn getenv @dev` and try again, ' +
-    'or re-run this command with `yarn serve --force yourService`.',
-  ));
-  process.exit(1);
-}
-
 if (!argv._.length) {
   console.log(colors.cyan(
     '[blinddeez.serve] No service specified, serving all services.'
